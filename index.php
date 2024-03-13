451e5540eb94e3b5f1e99fa23e9a969bf4834f49
William Eckerleben <ing@williameckerleben.com>
2024-03-13 16:08:23 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KB128NE8SR"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KB128NE8SR');
+</script>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>:|:.. Christian Riveros :|: info@criveros.com ..:|: </title><br />
+
+<?php
+       if (isset($_SERVER['HTTPS'])) {
+       
+} else {
+       echo '<meta http-equiv="Refresh" content="0;url=https://www.criveros.com">';
+     }
+?>
+<link href="style.css" rel="stylesheet" type="text/css" />
+
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-30171536-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+
+</head>
+
+<body>
+<div class="box" id="wrap_construction">
+  <div id="servicios">
+    <h3 align="center"><img src="gfx/default_new_junio.png" width="390" height="450" border="0" usemap="#Map" />
+<map name="Map" id="Map"><area shape="rect" coords="260,89,337,112" href="http://www.linkedin.com/in/criveroshq" target="_blank" alt="LinkedIN" />
+</map></h3>
+  </div>
+  <h3 align="center"><span class="ciudad">Asunci&oacute;n-Paraguay</span><br />
+  <a href="mailto:info@criveros.com">info@criveros.com</a></h3>
+</div>
+</body>
+</html>
